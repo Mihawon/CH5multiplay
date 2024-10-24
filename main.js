55140e16456c1c976d024141e7a4c81202d4b370
mihawon <kmghope7@gmail.com>
2024-10-24 15:11:31 +0900

    20241024

--- main.js
+++ main.js
@@ -1,7 +1,56 @@
 import net from 'net';
 import dotenv from 'dotenv';
+// proto
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import protobuf from 'protobufjs';
 
-//header
+const packetNames = {
+  common:{Packet:'common.Packet'},
+  initial:{InitalPayload:'initial.InitailPayload'},
+  game:{LocationUpdatePayload:'game.LocationUpdatePayload'},
+  response:{Response:'response.Response'},
+  gameNotification:{LocationUpdate:'gameNotification.locationUpdate'}
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const protoDir = path.join(__dirname,'../proto');
+const protoMessages = {};
+const getAllProtoFiles = function(dir,fileList=[]){
+  const files = fs.readdirSync(dir);
+
+  files.forEach(function(file){
+    const filePath = path.join(dir,file);
+    if(fs.statSync(filePath).isDirectory()){getAllProtoFiles(filePath,fileList);}
+    else if(path.extname(file)==='.proto'){fileList.push(filePath);}
+  });
+};
+const protoFiles =getAllProtoFiles(protoDir);
+
+const loadProtos = async function(){
+  try{
+    const root = new protobuf.Root();
+    await Promise.all(protoFiles.map((function(file){root.load(file);})));	 
+    for(const[packetName,types]of Object.entires(packetNames)){
+      protoMessages[packetName]={};
+      for(const[packetName,types]of Object.entires(packetName)){
+        protoMessages[packetName][type]=root.lookupType(typeName);
+      }
+    };
+    console.log('protobuf Loaded!');
+  }
+  catch(e){
+    console.error('protobuf Error!');	  
+  }	  
+};
+
+const getProtoMessages = function(){
+  return {...protoMessages};
+};
+
+// header
 const TOTAL_LENGTH=4;
 const PACKET_TYPE_LENGTH=1;
 const PACKET_TYPE={
@@ -9,8 +58,13 @@ const PACKET_TYPE={
   NORMAL:1,
   LOCATION:3
 };
+const HANDLER_IDS={
+  INITIAL:0,
+  LOCATION_UPDATE:2,
+}
+const RESPONSE_SUCESS_CODE=0;
 
-//event
+// event
 const onConnection=function(socket){
   console.log(`Cilent connected from: ${socket.remoteAdress}:${socket.remotePort}`);
   socket.on('data',onData(socket));
@@ -20,8 +74,12 @@ const onConnection=function(socket){
 const onData=function(socket){};
 const onEnd=function(socket){};
 const onError=function(socket){};
-
 const server = net.createServer(onConnection);
+
+// index 
+const initServer=async function(){
+  await loadProtos();
+};
 
 // env 
 dotenv.config();
@@ -29,8 +87,13 @@ const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || '5555';
 
 //check the connection
-server.listen(PORT, HOST, function(){
-  console.log(`Server is on ${HOST},${PORT}`);
+initServer().then(function(){
+  server.listen(PORT, HOST, function(){
+    console.log(`Server is on ${HOST},${PORT}`);
+  });
+}).catch(function(e){
+  console.error(e);
+  process.exit(1);
 });
 
 
